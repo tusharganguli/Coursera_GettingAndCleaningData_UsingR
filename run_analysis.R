a2e0cffb9d0b710c35dffdd93063da4a0451e25c
Tushar Ganguli <tushar.ganguli@gmail.com>
2017-08-09 08:01:25 -0600

    updated script file to include the relative working dir access and deleting extra variables from the environment.

--- run_analysis.R
+++ run_analysis.R
@@ -16,29 +16,29 @@ rm(list=ls())
 library(dplyr)
 
 # set the path to the project directory
-setwd("F:/online courses/coursera/DataScience-JohnHopkins/3 - Getting and Cleaning Data/project/getdata%2Fprojectfiles%2FUCI HAR Dataset/UCI HAR Dataset/")             
+setwd("F:/online courses/coursera/DataScience-JohnHopkins/3 - Getting and Cleaning Data/project/getdata%2Fprojectfiles%2FUCI HAR Dataset/")             
 
 # read the subject of each individual from subject_(train/test).txt
-subject_train <- read.table("train/subject_train.txt")
-subject_test <- read.table("test/subject_test.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
 subject <- rbind(subject_train, subject_test)
 rm(subject_test,subject_train)
 
 # read the corresponding activities performed.
-y_train <- read.table("train/y_train.txt")
-y_test <- read.table("test/y_test.txt")
-activity_labels <- rbind(y_train, y_test)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+activity_idx <- rbind(y_train, y_test)
 rm(y_test,y_train)
 
 # read the feature variables
-x_train <- read.table("train/X_train.txt")
-x_test <- read.table("test/X_test.txt")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
 feature_vector <- rbind(x_train, x_test)
 rm(x_train,x_test)
 
 
 # read features.txt and recover all indexes and column names for mean
-feature_names <- read.table("features.txt")
+feature_names <- read.table("UCI HAR Dataset/features.txt")
 mean_idx <- grep("*mean*",feature_names$V2)
 mean_colnames <- grep("*mean*",feature_names$V2, value = TRUE)
 # improve readability of column names
@@ -76,14 +76,21 @@ rm(std_idx,std_colnames)
 rm(feature_names)
 
 # create the single data set with all values
-mean_and_std <- cbind(subject,activity_labels,mean_values,std_values)
+mean_and_std <- cbind(subject,activity_idx,mean_values,std_values)
 colnames(mean_and_std)[1] <- "Subject"
 colnames(mean_and_std)[2] <- "Activity"
-rm(subject,activity_labels,feature_vector, mean_values,std_values)
+rm(subject,activity_idx,feature_vector, mean_values,std_values)
 
 # create a second, independent tidy data set with the average 
 # of each variable for each activity and each subject.
 averageData <- mean_and_std %>% group_by(Subject,Activity) %>% summarise_all(mean) 
+
+# update the category names for the Activity column
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+for(i in 1:6) {
+  averageData$Activity <- gsub(as.character(i),activity_labels$V2[i],averageData$Activity)
+}
+rm(activity_labels,i)
 
 # write the tidy data set to a text file
 write.table(averageData, file = "averageData.txt",row.names = FALSE)
